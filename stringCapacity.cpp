45ca08c02f01b0a349550bc17456978c4313a8f1
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-06-02 15:50:57 +0600

    Exploring string capacity

---
+++ stringCapacity.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string ourString = "Hello World";
+    // cout << ourString.size() << endl;
+    // cout << ourString.max_size() << endl;
+    cout << ourString.capacity() << endl;
+    
+    return 0;
+}
