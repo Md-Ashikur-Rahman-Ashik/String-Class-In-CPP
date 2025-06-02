f1d531f0763b09de981ad52926b51c8c4ccbecff
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-06-02 18:30:35 +0600

    Exploring string modifiers

---
+++ stringModifier.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string ourString = "Hello World ";
+    // string secondString = "Hi";
+    // ourString += secondString;
+    ourString.push_back('A');
+
+    cout << ourString;
+
+    return 0;
+}
