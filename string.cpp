a8006cbd9a8a86fdfb233598e6219f7a9b1a4a3c
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-06-02 15:42:20 +0600

    Exploring string in C++

---
+++ string.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // char stringArray[100] = "Hello";
+    // strcpy(stringArray, "AnotherHello");
+    // cout << stringArray;
+
+    string ourString = "Hello";
+    string newString = "AnotherHello";
+
+    if (ourString == newString)
+    {
+        cout << "Same";
+    }
+    else
+    {
+        cout << "Not Same";
+    }
+
+    return 0;
+}
