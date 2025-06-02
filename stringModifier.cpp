7bb3fe2898046aab3a227fe962879c2c40acca02
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-06-02 18:46:34 +0600

    Explored string modifiers

--- stringModifier.cpp
+++ stringModifier.cpp
@@ -6,7 +6,9 @@ int main()
     string ourString = "Hello World ";
     // string secondString = "Hi";
     // ourString += secondString;
-    ourString.push_back('A');
+    // ourString.push_back('A');
+    // ourString.erase(5);
+    ourString.replace(6, 5, "Bangladesh");
 
     cout << ourString;
 
