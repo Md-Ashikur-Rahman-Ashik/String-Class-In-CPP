4d69d77ba650a43b9ceea679578806338811a45b
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-06-02 18:09:42 +0600

    Explored string capacity funtions

--- stringCapacity.cpp
+++ stringCapacity.cpp
@@ -6,7 +6,22 @@ int main()
     string ourString = "Hello World";
     // cout << ourString.size() << endl;
     // cout << ourString.max_size() << endl;
-    cout << ourString.capacity() << endl;
-    
+    // cout << ourString.capacity() << endl;
+
+    // ourString.clear();
+
+    // if (ourString.empty() == true)
+    // {
+    //     cout << "This string is Empty";
+    // }
+    // else
+    // {
+    //     cout << "There is value in the string";
+    // }
+
+    ourString.resize(15, '0');
+
+    cout << ourString;
+
     return 0;
 }
