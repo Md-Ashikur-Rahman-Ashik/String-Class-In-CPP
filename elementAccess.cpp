8c828e155645154aa77ef7912f7aeaa2f91daad4
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-06-02 18:18:41 +0600

    Explored element access

---
+++ elementAccess.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string ourString;
+    cin >> ourString;
+    // cout << ourString.at(0);
+    cout << ourString.front() << endl;
+    cout << ourString.back() << endl;
+
+    return 0;
+}
